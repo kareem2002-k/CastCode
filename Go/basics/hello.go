9460a5e1048398969f211ba0a6aeaf26a27bb0a9
Kareem Ahmed <kareemsamir166@gmail.com>
2025-09-23 11:24:25 +0300

    update on go modules

--- Go/basics/hello.go
+++ Go/basics/hello.go
@@ -1,17 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"tut/greetings"
+    "fmt"
+    "log"
+
+    "tut/greetings"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message, err := greetings.Hello("")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(message)
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
 
+    // A slice of names.
+    names := []string{"Gladys", "Samantha", "Darrin"}
+
+    // Request greeting messages for the names.
+    messages, err := greetings.Hellos(names)
+    if err != nil {
+        log.Fatal(err)
+    }
+    // If no error was returned, print the returned map of
+    // messages to the console.
+    fmt.Println(messages)
 }
